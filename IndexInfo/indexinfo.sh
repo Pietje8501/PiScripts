7d4ad4836894ee107c8ff5fb5378944a027b61ac
root <root@raspberrypi.(none)>
2013-05-14 10:11:18 +0200

    changed

--- IndexInfo/indexinfo.sh
+++ IndexInfo/indexinfo.sh
@@ -4,40 +4,25 @@
 # system                           #
 ####################################
 #!/bin/bash
-indexfile='/var/www/index.html'
+statsfile='/var/www/data/system.txt'
 
-echo '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="stylesheet" type="text/css" href="style.css" >
-<title>Raspberry Pi Info</title>
-</head>
-<body>
-<div id="logo">
-</div>
-<div id="main">
-<div id="info">' > $indexfile
+temp=$(/opt/vc/bin/vcgencmd measure_temp | sed 's/.*=//' | sed "s/'.*//")
+echo $temp > $statsfile
 
-temp=$(/opt/vc/bin/vcgencmd measure_temp | sed 's/.*=//' | sed "s/'/\&deg;/")
-echo "De huidige temperatuur bedraagt: "$temp >> $indexfile
-echo "</div>" >> $indexfile
+cpuload=$(cat /proc/loadavg | awk '{ print $3 }')
+echo $cpuload >> $statsfile
 
-echo '<div id="load">' >> $indexfile
-cpuload=$(cat /proc/loadavg | awk '{ print $3"&#37;" }')
-echo "De gemiddelde CPU-load in de vorige 15min bedroeg: "$cpuload"<br>" >> $indexfile
-ramtotal=$(cat /proc/meminfo | awk 'NR==1 { print int($(NF-1)/1024)"MB" }')
-ramfree=$(cat /proc/meminfo | awk 'NR==2 { print int($(NF-1)/1024)"MB" }')
-echo "Er is nog "$ramfree" van de "$ramtotal" geheugen vrij" >> $indexfile
-echo "</div>" >> $indexfile
+ramtotal=$(cat /proc/meminfo | awk 'NR==1 { print int($(NF-1)/1024) }')
+echo $ramtotal >> $statsfile
+ramfree=$(cat /proc/meminfo | awk 'NR==2 { print int($(NF-1)/1024) }')
+echo $ramfree >> $statsfile
 
-echo '<div id="uptime">' >> $indexfile
-uptime=$(uptime | awk ' { print $3 } ' | sed 's/,//')
-echo "Het systeem is al "$uptime" online" >> $indexfile
-echo "</div>" >> $indexfile
+uptime=$(uptime | sed 's/.*up //' | sed 's/,[ \t]*[0-9][ \t]*user.*$//')
+echo $uptime >> $statsfile
 
-echo '</div>
-</body>
-</html>' >> $indexfile
+totaldisk=$(df | awk 'NR==2 { print int($2/1024) }')
+echo $totaldisk >> $statsfile
+useddisk=$(df | awk 'NR==2 { print int($3/1024) }')
+echo $useddisk >> $statsfile
 
+sh /home/pi/scripts/makepicture.sh
